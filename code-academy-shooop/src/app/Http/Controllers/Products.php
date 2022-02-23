74ab56f6167f10e6f984eacf0b4d3d0f69c2f57c
Andrew <90209255+AndzejusN@users.noreply.github.com>
2022-02-23 12:22:44 +0200

    fourth commmit

--- code-academy-shooop/src/app/Http/Controllers/Products.php
+++ code-academy-shooop/src/app/Http/Controllers/Products.php
@@ -5,13 +5,39 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models;
 
 class Products extends Controller
 {
-    public function show()
+    public function show(Request $request)
     {
-        $query = DB::table('products')->get();
+        $query = Models\Product::orderBy('id');
 
-        return view('shop.products', compact('query'));
+//        where('active', true);
+
+        if ($request->has('category') && is_numeric($request->category)) {
+            $query->where('category_id', $request->category);
+        }
+
+        if ($request->has('check') && is_numeric($request->check)) {
+            $query->where('active', $request->check);
+        }
+
+        if ($request->has('search') && is_string($request->search)) {
+            $query->where('model', 'LIKE', '%' . $request->search . '%');
+        }
+
+        $products = $query->get();
+
+        $categories = Models\Category::get();
+
+        $view = view('shop.products', compact('products', 'categories'));
+
+        $view->with('category_id', $request->category)->with('active', $request->check)->with('search_val', $request->search);
+
+        return $view;
     }
 }
+
+
+
--- code-academy-shooop/src/resources/views/shop/products.blade.php
+++ code-academy-shooop/src/resources/views/shop/products.blade.php
@@ -1,22 +1,32 @@
 @extends('shop.index')
 
 @section('content')
+    <div class="container">
+        <div class="row">
 
-        <table class ="table table-bordered">
-            <tbody>
-            <tr>
-                <th>Product:</th>
-                <th>Model:</th>
-                <th>Price:</th>
-    </tr>
-            @foreach ($query as $values)
-            <tr>
-                <td>{{$values->name}}</td>
-                <td>{{$values->model}}</td>
-                <td>{{$values->price}} Euro</td>
-            </tr>
-            @endforeach
-            </tbody>
-        </table>
+            @include('shop.partials.filteractive')
+
+            <div class="col">
+                <table class="table table-bordered">
+                    <tbody>
+                    <tr>
+                        <th>Product group:</th>
+                        <th>Model:</th>
+                        <th>Price:</th>
+                    </tr>
+
+                    @foreach ($products as $values)
+                        <tr>
+                            <td>{{$values->name}}</td>
+                            <td>{{$values->model}}</td>
+                            <td>{{$values->price}} Euro</td>
+                        </tr>
+                    @endforeach
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 
 @endsection
