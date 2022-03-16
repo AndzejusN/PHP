0bbed95d087f72967518af6320caaba9c3b7947c
Andrew <90209255+AndzejusN@users.noreply.github.com>
2022-03-16 19:18:59 +0200

    Vue manage / few first components plus

---
+++ code-academy-shooop/src/resources/js/shop/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HeaderView from './views/partials/HeaderView.vue';
+</script>
+
+<template>
+    <div class="container py-3">
+        <HeaderView/>
+        <main>
+            <h1>Hello world</h1>
+        </main>
+    </div>
+</template>
--- code-academy-shooop/src/webpack.mix.js
+++ code-academy-shooop/src/webpack.mix.js
@@ -6,9 +6,11 @@ if (mix.inProduction()) {
     mix.sourceMaps();
 }
 
-
-mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
+mix.js('resources/js/app.js', 'public/js/app.js').postCss('resources/css/app.css', 'public/css/app.css', [
     require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
 ]);
+
+mix.js('resources/js/shop/main.js', 'public/js/shop.js').vue();
+mix.sass('resources/sass/shop/app.scss','public/css/shop.css');
