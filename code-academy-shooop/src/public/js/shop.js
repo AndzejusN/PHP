dca2898d027c7e20ff96d15dd87ffc6c31a4824f
Andrew <90209255+AndzejusN@users.noreply.github.com>
2022-03-21 14:27:21 +0200

    2 filters plus / not fully good working

--- code-academy-shooop/src/public/js/shop.js
+++ code-academy-shooop/src/public/js/shop.js
@@ -19230,7 +19230,9 @@ __webpack_require__.r(__webpack_exports__);
     expose();
     var state = (0,vue__WEBPACK_IMPORTED_MODULE_0__.reactive)({
       products: [],
-      removableProductIndex: null
+      removableProductIndex: null,
+      search: '',
+      check: false
     });
 
     function loadProducts() {
@@ -19269,6 +19271,15 @@ __webpack_require__.r(__webpack_exports__);
       });
     }
 
+    function addSearchParameter(name, value) {
+      var paramsString = 'api/v1/products?';
+      var searchParams = new URLSearchParams();
+      searchParams.append(name, value);
+      state.check = false;
+      state.search = '';
+      loadProducts(paramsString + decodeURI(searchParams.toString()));
+    }
+
     var __returned__ = {
       state: state,
       loadProducts: loadProducts,
@@ -19276,7 +19287,7 @@ __webpack_require__.r(__webpack_exports__);
       removeSpecificProduct: removeSpecificProduct,
       filterProductsAscending: filterProductsAscending,
       filterProductsDescending: filterProductsDescending,
-      ref: vue__WEBPACK_IMPORTED_MODULE_0__.ref,
+      addSearchParameter: addSearchParameter,
       reactive: vue__WEBPACK_IMPORTED_MODULE_0__.reactive
     };
     Object.defineProperty(__returned__, '__isScriptSetup', {
@@ -19378,57 +19389,75 @@ var _hoisted_3 = {
   "class": "my-3"
 };
 var _hoisted_4 = {
-  "class": "row my-3"
+  "class": "row my-3 mx-3 w-25"
 };
-var _hoisted_5 = {
-  "class": "container"
-};
+
+var _hoisted_5 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("hr", null, null, -1
+/* HOISTED */
+);
+
 var _hoisted_6 = {
-  "class": "col-2"
+  "class": "container"
 };
 var _hoisted_7 = {
   "class": "row"
 };
 var _hoisted_8 = {
-  "class": "mt-5"
+  "class": "col-3 px-3"
+};
+var _hoisted_9 = {
+  "class": "row"
+};
+var _hoisted_10 = {
+  "class": "mt-3"
 };
 
-var _hoisted_9 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("label", {
+var _hoisted_11 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("label", {
   "for": "checkbox"
 }, "  Sell on-line", -1
 /* HOISTED */
 );
 
-var _hoisted_10 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
-  "class": "my-5"
-}, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("a", {
-  href: "http://localhost:8085/shop",
-  "class": "btn btn-outline-secondary"
-}, "Reset")], -1
+var _hoisted_12 = {
+  "class": "mt-5"
+};
+var _hoisted_13 = {
+  "class": "input-group"
+};
+
+var _hoisted_14 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
+  "class": "input-group-text"
+}, "Search", -1
 /* HOISTED */
 );
 
-var _hoisted_11 = {
-  "class": "col-9 d-flex flex-wrap"
+var _hoisted_15 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("label", {
+  "for": "search"
+}, null, -1
+/* HOISTED */
+);
+
+var _hoisted_16 = {
+  "class": "my-5"
 };
-var _hoisted_12 = {
-  "class": "input-group mb-3",
+var _hoisted_17 = {
+  "class": "col-8 d-flex flex-wrap mt-3 px-3"
+};
+var _hoisted_18 = {
+  "class": "row input-group",
   style: {
     "display": "inline-flex",
     "width": "auto"
   }
 };
-var _hoisted_13 = {
-  "class": "row mb-5"
-};
-var _hoisted_14 = {
+var _hoisted_19 = {
   "class": "card text-center",
   style: {
     "width": "17rem"
   }
 };
 
-var _hoisted_15 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("img", {
+var _hoisted_20 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("img", {
   "class": "card-img-top",
   src: "img/noimage.png",
   alt: "Some ..."
@@ -19436,19 +19465,19 @@ var _hoisted_15 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElement
 /* HOISTED */
 );
 
-var _hoisted_16 = {
+var _hoisted_21 = {
   "class": "card-body"
 };
-var _hoisted_17 = {
+var _hoisted_22 = {
   "class": "card-title"
 };
-var _hoisted_18 = {
+var _hoisted_23 = {
   "class": "card-text"
 };
-var _hoisted_19 = {
+var _hoisted_24 = {
   "class": "card-text"
 };
-var _hoisted_20 = {
+var _hoisted_25 = {
   "class": "card-text"
 };
 function render(_ctx, _cache, $props, $setup, $data, $options) {
@@ -19470,34 +19499,59 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
     onClick: $setup.filterProductsDescending
   }, "SORT DESCENDING PRODUCTS")]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_4, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.withDirectives)((0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("input", {
     type: "number",
-    "class": "form-control mx-3 my-3",
+    "class": "form-control my-3",
     "onUpdate:modelValue": _cache[1] || (_cache[1] = function ($event) {
       return $setup.state.removableProductIndex = $event;
     })
   }, null, 512
   /* NEED_PATCH */
   ), [[vue__WEBPACK_IMPORTED_MODULE_0__.vModelText, $setup.state.removableProductIndex]]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("button", {
-    "class": "btn btn-outline-danger mx-3",
+    "class": "btn btn-outline-danger",
     type: "button",
     onClick: _cache[2] || (_cache[2] = function ($event) {
       return $setup.removeSpecificProduct($setup.state.removableProductIndex);
     })
-  }, "DELETE BY ID ")])])]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_5, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_6, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_7, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_8, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("input", {
+  }, "DELETE BY ID ")])]), _hoisted_5]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_6, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_7, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_8, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_9, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_10, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.withDirectives)((0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("input", {
     type: "checkbox",
+    ref: "check",
     id: "checkbox",
-    onChange: _cache[3] || (_cache[3] = function ($event) {
-      return $setup.loadProducts('api/v1/products/?check=1');
+    name: "check",
+    value: "1",
+    "onUpdate:modelValue": _cache[3] || (_cache[3] = function ($event) {
+      return $setup.state.check = $event;
+    }),
+    onClick: _cache[4] || (_cache[4] = function ($event) {
+      return $setup.addSearchParameter('check', 1);
     })
-  }, null, 32
-  /* HYDRATE_EVENTS */
-  ), _hoisted_9]), _hoisted_10])]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_11, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_12, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_13, [((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(true), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.renderList)($setup.state.products, function (product, i) {
-    return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_14, [_hoisted_15, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_16, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("h5", _hoisted_17, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(product.name), 1
+  }, null, 512
+  /* NEED_PATCH */
+  ), [[vue__WEBPACK_IMPORTED_MODULE_0__.vModelCheckbox, $setup.state.check]]), _hoisted_11]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_12, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_13, [_hoisted_14, _hoisted_15, (0,vue__WEBPACK_IMPORTED_MODULE_0__.withDirectives)((0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("input", {
+    type: "text",
+    "class": "form-control",
+    placeholder: "Search by model...",
+    id: "search",
+    name: "search",
+    "onUpdate:modelValue": _cache[5] || (_cache[5] = function ($event) {
+      return $setup.state.search = $event;
+    }),
+    onChange: _cache[6] || (_cache[6] = function ($event) {
+      return $setup.addSearchParameter('search', $setup.state.search);
+    })
+  }, null, 544
+  /* HYDRATE_EVENTS, NEED_PATCH */
+  ), [[vue__WEBPACK_IMPORTED_MODULE_0__.vModelText, $setup.state.search]])])]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_16, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("a", {
+    "class": "btn btn-outline-secondary",
+    onClick: _cache[7] || (_cache[7] = function ($event) {
+      return $setup.loadProducts();
+    })
+  }, "Reset")])])]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_17, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_18, [((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(true), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.renderList)($setup.state.products, function (product, i) {
+    return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_19, [_hoisted_20, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_21, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("h5", _hoisted_22, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(product.name), 1
     /* TEXT */
-    ), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_18, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(product.id), 1
+    ), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_23, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(product.id), 1
     /* TEXT */
-    ), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_19, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(product.model), 1
+    ), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_24, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(product.model), 1
     /* TEXT */
-    ), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_20, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(product.price) + " Euro", 1
+    ), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_25, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(product.price) + " Euro", 1
     /* TEXT */
     )])]);
   }), 256
--- code-academy-shooop/src/resources/js/shop/views/products/list.vue
+++ code-academy-shooop/src/resources/js/shop/views/products/list.vue
@@ -1,10 +1,12 @@
 <script setup>
 
-import {ref, reactive} from 'vue';
+import {reactive} from 'vue';
 
 const state = reactive({
     products: [],
-    removableProductIndex: null
+    removableProductIndex: null,
+    search: '',
+    check: false
 })
 
 
@@ -34,6 +36,21 @@ function filterProductsDescending() {
     state.products = state.products.sort((a, b) => a.id > b.id ? -1 : 1);
 }
 
+
+function addSearchParameter(name, value) {
+
+    const paramsString = 'api/v1/products?'
+
+    let searchParams = new URLSearchParams();
+    searchParams.append(name, value);
+
+    state.check = false;
+    state.search = '';
+
+    loadProducts(paramsString + decodeURI(searchParams.toString()));
+
+}
+
 </script>
 
 <template>
@@ -47,30 +64,42 @@ function filterProductsDescending() {
                 <button class="btn btn-primary mx-3" @click="filterProductsAscending">SORT ASCENDING PRODUCTS</button>
                 <button class="btn btn-info" @click="filterProductsDescending">SORT DESCENDING PRODUCTS</button>
             </div>
-            <div class="row my-3">
-                <input type="number" class="form-control mx-3 my-3" v-model="state.removableProductIndex">
-                <button class="btn btn-outline-danger mx-3" type="button"
+            <div class="row my-3 mx-3 w-25">
+                <input type="number" class="form-control my-3" v-model="state.removableProductIndex">
+                <button class="btn btn-outline-danger" type="button"
                         @click="removeSpecificProduct(state.removableProductIndex)">DELETE BY ID
                 </button>
             </div>
         </div>
+        <hr>
     </div>
+
     <div class="container">
-        <div class="col-2">
-            <div class="row">
-                <div class="mt-5">
-                    <input type="checkbox" id="checkbox"
-                           @change="loadProducts('api/v1/products/?check=1')"/>
-                    <label for="checkbox">&nbsp Sell on-line</label>
-                </div>
-                <div class="my-5">
-                    <a href="http://localhost:8085/shop" class="btn btn-outline-secondary">Reset</a>
+        <div class="row">
+            <div class="col-3 px-3">
+                <div class="row">
+                    <div class="mt-3">
+                        <input type="checkbox" ref="check" id="checkbox" name="check" value="1"
+                               v-model="state.check"
+                               @click="addSearchParameter('check', 1)"/>
+                        <label for="checkbox">&nbsp Sell on-line</label>
+                    </div>
+                    <div class="mt-5">
+                        <div class="input-group">
+                            <div class="input-group-text">Search</div>
+                            <label for="search"></label>
+                            <input type="text" class="form-control" placeholder="Search by model..." id="search"
+                                   name="search" v-model="state.search"
+                                   @change="addSearchParameter('search', state.search)">
+                        </div>
+                    </div>
+                    <div class="my-5">
+                        <a class="btn btn-outline-secondary" @click="loadProducts()">Reset</a>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-9 d-flex flex-wrap">
-            <div class="input-group mb-3" style="display:inline-flex; width:auto;">
-                <div class="row mb-5">
+            <div class="col-8 d-flex flex-wrap mt-3 px-3">
+                <div class="row input-group" style="display:inline-flex; width:auto;">
                     <div class="card text-center" v-for="(product,i) in state.products" style="width: 17rem;">
                         <img class="card-img-top" src="img/noimage.png" alt="Some ...">
                         <div class="card-body">
